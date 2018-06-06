1ad288fc58ebc883beb3058eb36a1bf48f9a3d41
rorsach <rorsach@olmdigital.com>
2018-06-06 00:10:20 -0700

    basic webpack bundling setup

--- src/digits.js
+++ src/digits.js
@@ -1,6 +1,8 @@
-class Digits extends React.Component {
+import React from 'react'
+export class Digits extends React.Component {
     render() {
         return React.createElement('div', null, `${this.props.sign} ${this.props.value}`)
     }
 }
+
                                    
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Digits} from './digits'
+
 ReactDOM.render(
     React.createElement(Digits, {sign: '-', value: '397'}, null),
     document.getElementById('root')
-);
+)
