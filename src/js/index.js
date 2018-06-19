436ee905d1b5b05cf122e28dab6eb37201438838
rorsach <rorsach@olmdigital.com>
2018-06-19 00:43:54 -0700

    Add configuration menu css and animation.

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import {NumberFactory} from './NumberFactory'
 import {ProblemPage} from './ProblemPage'
 import '../css/style.css'
 
-var numbers = new NumberFactory(1, 7, 6).generateNumbers()
+let numbers = new NumberFactory(3, 7, 8).generateNumbers()
 
 
 ReactDOM.render(
